e4fe94c91c17b9fdf8aaa3e2e82e0658de47335f
Kris Immig <kris@factorial.io>
2019-06-18 11:20:03 +0200

    Run tasks in parallel with new core setup.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,6 +1,8 @@
 module.exports = {
   use: [
-    "@factorial/frontend-stack-core",
+    // "@factorial/frontend-stack-core",
+    // This must point to the frontend-stack on branch 'feature/dev-server-with-linting'
+    "../factorial-frontend-stack/packages/core",
     "@factorial/frontend-stack-postcss-export-custom-variables"
   ],
   options: {
