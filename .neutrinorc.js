f57519e6a2265065dc9f23903d8f3c6399e7bb86
Kris Immig <kris@factorial.io>
2019-06-18 15:01:14 +0200

    Revert temp dev changes.

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,8 +1,6 @@
 module.exports = {
   use: [
-    // "@factorial/frontend-stack-core",
-    // This must point to the frontend-stack on branch 'feature/dev-server-with-linting'
-    "../factorial-frontend-stack/packages/core",
+    "@factorial/frontend-stack-core",
     "@factorial/frontend-stack-postcss-export-custom-variables"
   ],
   options: {
